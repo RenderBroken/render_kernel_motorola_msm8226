711d539ae9719c38aea1eaedfe70116ddef87c2e
Dennis Rassmann <showp1984@gmail.com>
2012-12-19 11:50:58 +0100

    cpuinit: get rid of __cpuinit, manual fixups
    a few final fixups that haven't been caught by regexps
    
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	arch/x86/kernel/cpu/scattered.c
    	kernel/rcutree.h
    	kernel/rcutree_plugin.h

--- kernel/rcutree_plugin.h
+++ kernel/rcutree_plugin.h
@@ -948,7 +948,7 @@ EXPORT_SYMBOL_GPL(rcu_barrier);
 /*
  * Initialize preemptible RCU's per-CPU data.
  */
-static void __cpuinit rcu_preempt_init_percpu_data(int cpu)
+static void rcu_preempt_init_percpu_data(int cpu)
 {
 	rcu_init_percpu_data(cpu, &rcu_preempt_state, 1);
 }
@@ -1191,7 +1191,7 @@ EXPORT_SYMBOL_GPL(rcu_barrier);
  * Because preemptible RCU does not exist, there is no per-CPU
  * data to initialize.
  */
-static void __cpuinit rcu_preempt_init_percpu_data(int cpu)
+static void rcu_preempt_init_percpu_data(int cpu)
 {
 }
 
@@ -1435,7 +1435,7 @@ static void rcu_preempt_boost_start_gp(struct rcu_node *rnp)
  * already exist.  We only create this kthread for preemptible RCU.
  * Returns zero if all is well, a negated errno otherwise.
  */
-static int __cpuinit rcu_spawn_one_boost_kthread(struct rcu_state *rsp,
+static int rcu_spawn_one_boost_kthread(struct rcu_state *rsp,
 						 struct rcu_node *rnp)
 {
 	int rnp_index = rnp - &rsp->node[0];
@@ -1590,7 +1590,7 @@ static int __init rcu_spawn_kthreads(void)
 }
 early_initcall(rcu_spawn_kthreads);
 
-static void __cpuinit rcu_prepare_kthreads(int cpu)
+static void rcu_prepare_kthreads(int cpu)
 {
 	struct rcu_data *rdp = per_cpu_ptr(rcu_state->rda, cpu);
 	struct rcu_node *rnp = rdp->mynode;
@@ -1632,7 +1632,7 @@ static int __init rcu_scheduler_really_started(void)
 }
 early_initcall(rcu_scheduler_really_started);
 
-static void __cpuinit rcu_prepare_kthreads(int cpu)
+static void rcu_prepare_kthreads(int cpu)
 {
 }
 
