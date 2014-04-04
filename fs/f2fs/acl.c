b04e72b9f9d1ff70206394e455b40de61b63cc14
ZhangZhen <zhenzhang.zhang@huawei.com>
2014-04-04 09:47:16 +0800

    f2fs: check the acl's validity before setting
    Before setting the acl, call posix_acl_valid() to check if it is
    valid or not.
    
    Signed-off-by: zhangzhen <zhenzhang.zhang@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>
    
    Conflicts:
    	fs/f2fs/acl.c

--- fs/f2fs/acl.c
+++ fs/f2fs/acl.c
@@ -204,6 +204,12 @@ static int f2fs_set_acl(struct inode *inode, int type,
 	if (S_ISLNK(inode->i_mode))
 		return -EOPNOTSUPP;
 
+	if (acl) {
+		error = posix_acl_valid(acl);
+		if (error < 0)
+			return error;
+	}
+
 	switch (type) {
 	case ACL_TYPE_ACCESS:
 		name_index = F2FS_XATTR_INDEX_POSIX_ACL_ACCESS;
