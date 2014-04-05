b7e192500213a6e1f86e1b3f4b7d3ec20b4b235f
Pranav Vashi <neobuddy89@gmail.com>
2014-04-05 21:19:09 +0530

    staging:android:lmk: Use for_each_thread helper
    * Based on Faux's commit: https://github.com/faux123/Nexus_5/commit/2b75ffef20ecaee344726461b1ed3c0e2752b077
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -101,7 +101,7 @@ static unsigned long lowmem_deathpending_timeout;
 
 static int test_task_flag(struct task_struct *p, int flag)
 {
-	struct task_struct *t = p;
+	struct task_struct *t;
 
 	for_each_thread(p,t) {
 		task_lock(t);
